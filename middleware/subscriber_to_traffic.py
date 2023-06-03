4cbf669586106abcf01804381817caa5a7055be8
Kninja99 <kcdamschen@yahoo.com>
2023-06-02 23:04:40 -0700

    middle ware for gcloud(test) and traffic

--- middleware/subscriber_to_traffic.py
+++ middleware/subscriber_to_traffic.py
@@ -2,20 +2,24 @@ import zmq
 import time
 
 # setup for subscriber
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.connect('tcp://192.168.254.129:2000')
+context_subscriber = zmq.Context()
+socket_subscriber = context_subscriber.socket(zmq.SUB)
+socket_subscriber.connect('tcp://192.168.254.129:2000')
 # socket channel for publisher of traffic light
-socket.setsockopt_string(zmq.SUBSCRIBE, '10011')
+socket_subscriber.setsockopt_string(zmq.SUBSCRIBE, '10011')
+
+# setup publisher middleware to jetbot
+context_publisher = zmq.Context()
+socket_publisher = context_publisher.socket(zmq.PUB)
+socket_publisher.bind('tcp://*:2001')
+topic_publisher = 11000
 
 # logic for reading msgs
 
 while(True):
-    try: 
-        # if there is no message this get hung up
-        message = socket.recv()
+    time.sleep(1)
+    # if there is no message this get hung up
+    message = socket_subscriber.recv()
+    if len(message) > 0:
+        socket_publisher.send_string('%d %s' % (topic_publisher, message))
         print(message)
-    except:
-        print('no msg')
-        context.destroy()
-        time.sleep(1)
