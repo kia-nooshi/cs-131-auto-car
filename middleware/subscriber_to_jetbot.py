723648c84b85b704f7030027a82d75f83945f2ca
Kninja99 <kcdamschen@yahoo.com>
2023-06-03 01:25:06 -0700

    uncommented with wrapper

--- middleware/subscriber_to_jetbot.py
+++ middleware/subscriber_to_jetbot.py
@@ -60,16 +60,16 @@ def cloud_to_jetbot():
     print(f"Listening for messages on {subscription_path}..\n")
 
     # Wrap subscriber in a 'with' block to automatically call close() when done.
-    #with subscriber:
-    #    try:
-    #        # When `timeout` is not set, result() will block indefinitely,
-    #        # unless an exception is encountered first.
-    #        streaming_pull_future.result(timeout=timeout)
-    #    except TimeoutError:
-    #        streaming_pull_future.cancel()  # Trigger the shutdown.
-    #        streaming_pull_future.result()  # Block until the shutdown is complete.
-    #    except KeyboardInterrupt:
-    #        streaming_pull_future.cancel(await_msg_callbacks=True)  # blocks until done
+    with subscriber:
+        try:
+            # When `timeout` is not set, result() will block indefinitely,
+            # unless an exception is encountered first.
+            streaming_pull_future.result(timeout=timeout)
+        except TimeoutError:
+            streaming_pull_future.cancel()  # Trigger the shutdown.
+            streaming_pull_future.result()  # Block until the shutdown is complete.
+        except KeyboardInterrupt:
+            streaming_pull_future.cancel(await_msg_callbacks=True)  # blocks until done
 
 def cloud_sub():
     while True:
